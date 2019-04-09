c089982070306266345e6c34e5a0cab13ef4c1de
Clarisse CHARUEL <ccharuel@e1r6p12.42.fr>
2019-04-09 18:13:18 +0200

    start Sully

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,9 +1,31 @@
 section .data
-s db "Toto"
+s db "TOTOOTTOOTO", 0
+file db "Sully_%d.s", 0
+opt db "w+"
+
+
+section .bss
+buffer: resb 16
 
 section .text
-global _start
-extern _asprintf
+global _main
+extern _fopen
+extern _fprintf
+extern _system
+extern _sprintf
+
 _main:
 push rbp
 mov rbp, rsp
+lea rdi, [rel buffer] 
+lea rsi, [rel file]
+mov rdx, 5
+call  _sprintf
+lea rdi, [rel buffer]
+lea rsi, [rel opt]
+call _fopen
+mov rdi, rax
+lea rsi, [rel s]
+call _fprintf
+leave
+ret
