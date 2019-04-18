ed51cd598ffde43a786b2384d7d6d790264576f3
Clarisse CHARUEL <ccharuel@e1r6p12.42.fr>
2019-04-18 15:14:26 +0200

    rectification

--- JS/Sully.js
+++ JS/Sully.js
@@ -2,7 +2,9 @@ tqt=()=> {
 	const { exec } = require('child_process');
 	var i = 5;
 	if (i === 0) return;
+	i--;
 	var child = "Sully_"+i+".js";
+	i++;
 	require('fs').writeFileSync(child, 'tqt='+tqt.toString().replace(i, i - 1)+'\ntqt();\n');
 	i--;
 	exec("node "+child);
